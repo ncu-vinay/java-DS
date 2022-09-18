7c620d1902c47812a01422dd6651171e8e8b8249
Vinay Kumar <101244361+ncu-vinay@users.noreply.github.com>
2022-09-18 18:49:53 +0530

    Linear search

--- lo.java
+++ lo.java
@@ -1,31 +1,33 @@
-import java.util.*;
-class Main
-{
-public static void main(String args[])
-{
-int large,small,i;
-int n;  
-Scanner sc=new Scanner(System.in);  
-System.out.print("Enter the number of elements you want to store: ");  
-n=sc.nextInt();  
-int[] arr = new int[n];  
-System.out.println("Enter the elements of the array: ");  
-for(int j=0; j<n; j++)  
-{    
-arr[j]=sc.nextInt();  
+import java.util.Scanner;  
+   
+class Linear
+{  
+  public static void main(String args[])  
+  {  
+    int c, n, search, array[];  
+   
+    Scanner in = new Scanner(System.in);  
+    System.out.println("Enter number of elements");  
+    n = in.nextInt();   
+    array = new int[n];  
+   
+    System.out.println("Enter those " + n + " elements");  
+   
+    for (c = 0; c < n; c++)  
+      array[c] = in.nextInt();  
+   
+    System.out.println("Enter value to find");  
+    search = in.nextInt();  
+   
+    for (c = 0; c < n; c++)  
+    {  
+      if (array[c] == search)     /* Searching element is present */  
+      {  
+         System.out.println(search + " is present at location " + (c + 1) + ".");  
+          break;  
+      }  
+   }  
+   if (c == n)  /* Element to search isn't present */  
+      System.out.println(search + " isn't present in array.");  
+  }  
 }  
-
-large=small=arr[0];
-for(i=1;i<n;++i)
-{
-if(arr[i]>large)
-large=arr[i];
-
-if(arr[i]<small)
-small=arr[i];
-}
-
-int diff = large - small;
-System.out.print("Difference between large and small are: " + diff);
-}
-}
